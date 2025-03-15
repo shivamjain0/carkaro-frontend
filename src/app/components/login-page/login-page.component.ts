67c34e41726cd0edd47701e32f82f38680989f92
Shivam jain <shivamjain946565@gmail.com>
2025-03-15 18:11:34 +0530

    Code Optimization

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -143,6 +143,7 @@ export class LoginPageComponent implements OnInit {
         complete: () => {
           this.route.navigate(['login'], { queryParams: { isLogin: true } });
           alert('User created successfully');
+          
         },
       });
     }
